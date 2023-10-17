dfb83f72648d5ed541837ee425cc9d52af31661f
ellenmariadev <ellenmariadev@gmail.com>
2023-10-17 19:01:55 -0300

    chore: dependencies + layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+};
+
+module.exports = nextConfig;
