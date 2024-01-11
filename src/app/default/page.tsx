3f850f1ec7738cd38991bd6e37acee15461b6fbf
ellenmariadev <ellenmariadev@gmail.com>
2024-01-11 00:34:28 -0300

    feat(page): default

---
+++ src/app/default/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import S from './styles.module.scss';
+
+const Default = () => {
+  return (
+    <div className={S.default}>
+      <h1>Aguardando aprovação...</h1>
+    </div>
+  );
+};
+
+export default Default;
