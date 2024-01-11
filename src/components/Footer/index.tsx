cdfdb53a162b905c4a9707b9d554a850de1037f5
ellenmariadev <ellenmariadev@gmail.com>
2024-01-11 00:25:20 -0300

    feat(components): Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import S from './styles.module.scss';
-import { SiFacebook, SiInstagram } from 'react-icons/si';
-import { GoMail } from 'react-icons/go';
-import UfapeLMTS from '@/assets/ufape-lmts.svg';
 import Image from 'next/image';
+import React from 'react';
+import { GoMail } from 'react-icons/go';
+import { SiFacebook, SiInstagram } from 'react-icons/si';
+
+import S from './styles.module.scss';
+
+import { Icons } from '@/assets';
+import UfapeLMTS from '@/assets/ufape-lmts.svg';
 
 const Footer = () => {
   return (
     <footer className={S.footer}>
-      <ul role="list">
-        <li>Logo</li>
-        <li>
+      <ul>
+        <li className={S.logo}>
+          <Image
+            src={Icons.LogoWhite}
+            className={S.logoImg}
+            alt="Logo Gestão"
+          />{' '}
+          <p>Gestão</p>
+        </li>
+        <li className={S.ufapelmtsWrapper}>
           <Image
             className={S.ufapeLmts}
             src={UfapeLMTS}
